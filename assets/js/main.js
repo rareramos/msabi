07da58fc9a52d84df277656fe3b89b811d4c9132
rareramos <rareramos@outlook.ph>
2021-04-01 08:41:34 -0700

    update scroll-magic effect and fade out logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+  let lastKnownScrollPosition = 0;
+  let ticking = false;
   const time = 800;
+  const angle = 23;
   const logo = document.getElementById('logo');
   logo.addEventListener('click', function () {
     // hide logo
-    console.log(logo.children[0], 'logo')
     logo.children[0].style.display = 'none'
     logo.children[1].style.display = 'block'
     setTimeout(() => {
@@ -12,9 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }, time * 1.5);
 
     // move circle
-    const menu = document.getElementById('menu');
     const menuInner = document.getElementsByClassName('menu-inner')[0];
-    menu.style.transform = 'translate(-66%, -50%) rotate(0deg)';
     menuInner.style.transform = 'translate(0%, -50%) rotate(0deg)';
 
 
@@ -23,41 +23,69 @@ document.addEventListener('DOMContentLoaded', function () {
     const labelText = document.getElementsByClassName('label-text');
     const labelTextImage = document.getElementsByClassName('label-text-image');
     //  move label
-    const moveLabel = (rotateAngle, current) => {
+
+    const moveLabel = (rotateAngle) => {
       let setRotate = rotateAngle;
       // setTimeout(() => {
       for (let i = 0; i < pseudoMenu.length; i++) {
-        rotateAngle += 15;
-        console.log(rotateAngle, 'rotateAngle')
+        rotateAngle += angle;
+
         pseudoMenu[i].style.transform = `rotate(${-rotateAngle}deg)`;
         pseudoMenu[i].style.opacity = '1';
+
+        const zooming = () => {
+          const menuCorePoint = document.getElementById('menu-core');
+          const menuCorePointPosition = menuCorePoint.getBoundingClientRect();
+          const number = Math.abs((label[i].getBoundingClientRect().y - menuCorePointPosition.y) * 0.058);
+
+          // change color
+          if (number > 0 && number < 3) {
+            label[i].children[0].style.backgroundColor = '#2069FC'
+            label[i].children[1].style.backgroundColor = '#2069FC'
+          } else {
+            label[i].children[0].style.backgroundColor = '#0f5e9e'
+            label[i].children[1].style.backgroundColor = '#0f5e9e'
+
+          }
+          return pseudoMenu[i].style.fontSize = `${16 + 17 - number}px`;
+        }
+        zooming();
       }
 
       //  move text label
       rotateAngle = setRotate;
       for (let i = 0; i < labelText.length; i++) {
-        rotateAngle += 15;
-        label[i].style.transform = `translate(100%, -50%) rotate(${rotateAngle}deg) scale(1)`;
-        // label[i].style.transformOrigin = `inherit`;
-        if (current === i) {
-          label[i].style.transform = `translate(100%, -50%) rotate(${rotateAngle}deg) scale(1.35)`;
-          // label[i].style.transformOrigin = `left`;
-        }
-        setTimeout(() => {
-          labelText[i].style.width = '12.625em';
-          labelTextImage[i].style.opacity = '1'
-        }, time / 2)
+        rotateAngle += angle;
+        label[i].style.transform = `translate(100%, -50%) rotate(${rotateAngle}deg)`;
+        // setTimeout(() => {
+        labelText[i].style.width = '12.625em';
+        labelTextImage[i].style.opacity = '1'
+        // }, time / 2)
       }
       // }, time)
     }
-    moveLabel(-60, 3);
+    moveLabel(-(angle * pseudoMenu.length));
 
     // body height
-    console.log(document.body)
     document.body.style.height = `${(pseudoMenu.length - 1) * 100}vh`;
 
+    function doSomething(scrollPos) {
+      // move label (responsive mode)
+      const scrollHeight = document.body.offsetHeight - window.innerHeight;
+      const scrolling = scrollPos / ((scrollHeight) / ((pseudoMenu.length - 1) * angle));
+      moveLabel(-(angle * pseudoMenu.length) + scrolling);
+    }
+
+
     document.addEventListener('scroll', function () {
-      moveLabel(-60 + window.scrollY / 20, 3);
+      lastKnownScrollPosition = window.scrollY;
+      if (!ticking) {
+        window.requestAnimationFrame(function () {
+          doSomething(lastKnownScrollPosition);
+          ticking = false;
+        });
+        ticking = true;
+      }
 
     })
 
